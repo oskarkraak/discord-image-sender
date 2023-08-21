dddd7e7f779856f0b8699c741f67b548aacc878d
morit <moritz.herrklotsch@gmail.com>
2023-08-21 21:30:31 +0200

    Implement webhook

--- src/main/java/com/oskarkraak/Main.java
+++ src/main/java/com/oskarkraak/Main.java
@@ -1,7 +1,32 @@
 package com.oskarkraak;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.OutputStream;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static final String IMAGE_URL = "Empty";
+    public static final String WEBHOOK_URL = "Empty";
+
+    public static void main(String[] args) throws Exception {
+        URL url = new URL(WEBHOOK_URL);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.addRequestProperty("Content-Type", "application/json");
+        connection.addRequestProperty("User-Agent", "discord-image-sender");
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        String json = """
+                {
+                  "content": "%s",
+                   "embeds": null,
+                   "attachments": []
+                }
+                """.formatted(IMAGE_URL);
+        OutputStream stream = connection.getOutputStream();
+        stream.write(json.getBytes());
+        stream.flush();
+        stream.close();
+        System.out.println( connection.getInputStream());
+        connection.disconnect();
     }
 }
