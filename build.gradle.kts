0ab5aeba8fa91e3486d82f6ac769999317c7449c
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2023-09-02 16:00:58 +0200

    Add AWS CDK dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.94.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
