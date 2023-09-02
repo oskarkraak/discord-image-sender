b1c26cd989dc6db8cc0c6c9f83af99afe5d3b139
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2023-09-02 16:24:05 +0200

    Add AWS Java Lambda dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("software.amazon.awscdk:aws-cdk-lib:2.94.0")
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
