bd714a6ee6f27623bc71aaa60a4f25495a1e8b53
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2023-09-10 12:56:41 +0200

    Fix Lambda code and handler file paths

--- infrastructure/src/main/java/com/oskarkraak/discordimagesender/ImageSenderApp.java
+++ infrastructure/src/main/java/com/oskarkraak/discordimagesender/ImageSenderApp.java
@@ -10,8 +10,8 @@ public class ImageSenderApp {
     public static void main(final String[] args) {
         App app = new App();
         Schedule imageSendingSchedule = Schedule.expression("* * * * ?"); // TODO schedule
-        Code lambdaCode = Code.fromAsset("../../"); // TODO code path
-        String handler = "package::Handler"; // TODO handler
+        Code lambdaCode = Code.fromAsset("../software/build/libs/software-1.0.jar");
+        String handler = "com.oskarkraak.discordimagesender.ImageSenderLambda::handleRequest";
         Map<String, String> environment = Map.of(); // TODO env
         new ScheduledLambdaStack(app, "id", lambdaCode, handler, imageSendingSchedule, environment); // TODO id
         app.synth();
