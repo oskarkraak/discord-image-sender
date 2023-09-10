a3b0f1aaf508ad2a97008c3167673bc30af3c3a3
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2023-09-10 12:55:05 +0200

    Create Lambda and schedule in ScheduledLambdaStack

--- infrastructure/src/main/java/com/oskarkraak/discordimagesender/ImageSenderApp.java
+++ infrastructure/src/main/java/com/oskarkraak/discordimagesender/ImageSenderApp.java
@@ -4,13 +4,16 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.services.events.Schedule;
 import software.amazon.awscdk.services.lambda.Code;
 
+import java.util.Map;
+
 public class ImageSenderApp {
     public static void main(final String[] args) {
         App app = new App();
         Schedule imageSendingSchedule = Schedule.expression("* * * * ?"); // TODO schedule
         Code lambdaCode = Code.fromAsset("../../"); // TODO code path
         String handler = "package::Handler"; // TODO handler
-        new ScheduledLambdaStack(app, "id", lambdaCode, handler, imageSendingSchedule); // TODO id
+        Map<String, String> environment = Map.of(); // TODO env
+        new ScheduledLambdaStack(app, "id", lambdaCode, handler, imageSendingSchedule, environment); // TODO id
         app.synth();
     }
 }
--- infrastructure/src/main/java/com/oskarkraak/discordimagesender/ScheduledLambdaStack.java
+++ infrastructure/src/main/java/com/oskarkraak/discordimagesender/ScheduledLambdaStack.java
@@ -1,13 +1,28 @@
 package com.oskarkraak.discordimagesender;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.services.events.targets.LambdaFunction;
 import software.amazon.awscdk.services.lambda.*;
 import software.amazon.awscdk.services.events.*;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 
+import java.util.Map;
+
 public class ScheduledLambdaStack extends Stack {
-    public ScheduledLambdaStack(final Construct scope, final String id, Code code, String handler, Schedule schedule) {
+    public ScheduledLambdaStack(final Construct scope, final String id, Code code, String handler, Schedule schedule, Map<String, String> environment) {
         super(scope, id);
-        // TODO
+        Function lambdaFunction = Function.Builder.create(this, "scheduled-lambda")
+                .code(code)
+                .handler(handler)
+                .timeout(Duration.seconds(15))
+                .runtime(Runtime.JAVA_17)
+                .environment(environment)
+                .build();
+        Rule rule = Rule.Builder.create(this, "schedule")
+                .schedule(schedule)
+                .build();
+        rule.addTarget(new LambdaFunction(lambdaFunction));
     }
 }
